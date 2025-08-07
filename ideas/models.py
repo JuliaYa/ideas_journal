7892c0f488b4deaf7cd6fd61211552586d40ac89
JuliaYa <julia.yakutova@gmail.com>
2025-08-07 12:54:01 +0300

    Add Idea model and add migration

--- ideas/models.py
+++ ideas/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 
-class Status(models.Model):
-    name = models.CharField()
-    description = models.TextField()
+from model_utils.fields import StatusField
+from model_utils import Choices
+
 
 class Idea(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     created_at = models.DateTimeField("date published")
     updated_at = models.DateTimeField()
-    status = Status
+    STATUS = Choices('new', 'in_progress', 'done', 'archived')
 
-# как устанавливать дефолтные статусы? Я хочу добавлять сама типы статусов по моим идеям.
+# как устанавливать дефолтные статусы? Я хочу ли добавлять сама типы статусов по моим идеям.
 # Но по умолчанию должно быть сразу три доступных (New, In Progress, Done)
 
