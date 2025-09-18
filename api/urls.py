016a15f6044485f2a1258fdcf8947ecfd7ecfcb2
JuliaYa <julia.yakutova@gmail.com>
2025-09-18 12:48:46 +0300

    Add create and details endpoints to api.

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,7 @@ from ideas.views import IdeaViewSet
 
 router = routers.DefaultRouter()
 router.register(r'ideas', IdeaViewSet)
+router.register(r'ideas/<int:idea_id>', IdeaViewSet, basename='idea_details')    
+router.register(r'ideas/create', IdeaViewSet, basename='create_idea')
 
 urlpatterns = router.urls
--- ideas/views.py
+++ ideas/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import Http404
 from rest_framework import viewsets
 from .serializers import IdeaSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Idea
 
@@ -20,3 +23,25 @@ def details(request, idea_id):
     except Idea.DoesNotExist:
         raise Http404("Idea does not exist")
     return render(request, "ideas/details.html", {"idea": idea})
+
+@api_view(['GET'])
+def idea_details(idea_id):
+    try:
+        idea = Idea.objects.get(pk=idea_id)
+    except Idea.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    serializer = IdeaSerializer(idea)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def create_idea(request):
+    
+    serializer = IdeaSerializer(data=request.data)
+    
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
