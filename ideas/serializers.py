8998b7f043073158b4a59dee79939dc980a8f592
JuliaYa <julia.yakutova@gmail.com>
2026-04-08 14:30:46 +0300

    Save images for ideas.

--- ideas/serializers.py
+++ ideas/serializers.py
@@ -2,6 +2,14 @@ from rest_framework import serializers
 from .models import Idea
 
 class IdeaSerializer(serializers.ModelSerializer):
+    main_picture = serializers.ImageField(required=False, allow_null=True, default=None)
+
     class Meta:
         model = Idea
         fields = '__all__'
+
+    def to_internal_value(self, data):
+        if 'main_picture' in data and data['main_picture'] == '':
+            data = data.copy()
+            data['main_picture'] = None
+        return super().to_internal_value(data)
